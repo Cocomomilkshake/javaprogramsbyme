c5d240725fc9c105fa75aff27aa3821549b4711e
Cocomomilkshake <76200257+Cocomomilkshake@users.noreply.github.com>
2023-10-07 18:20:05 +0500

    Create Array1.java
    made an array script

---
+++ Array1.java
@@ -0,0 +1,15 @@
+public class Array1{
+  int array1e = [0,2,4,6,7,8,10];
+  // e = even
+  public static void main(String[] args){
+    // prints all of the elements of array1e:
+    System.out.println(array1e[0]);
+    System.out.println(array1e[1]);
+    System.out.println(array1e[2]);
+    System.out.println(array1e[3]);
+    System.out.println(array1e[4]);
+    System.out.println(array1e[5]);
+    System.out.println(array1e[6]);
+    System.out.println(array1e[7]);
+  }
+}
