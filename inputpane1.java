b7992be4c88d01a29be772d8e6cce2edd4a56031
Cocomomilkshake <76200257+Cocomomilkshake@users.noreply.github.com>
2023-10-23 08:19:02 -0700

    new stuff

--- inputpane1.java
+++ inputpane1.java
@@ -1,7 +1,19 @@
 import javax.swing.JOptionPane;
 public class pane1{
   public static void main(String[] args){
-    String name = JOptionPane.showInputDialog("What's Your Name?");
+    //convo 1:
+    String name1 = JOptionPane.showInputDialog("What's Your Name?");
     JOptionPane.showMessageDialog(null, "Hello " + name);
+    
+    // convo 2:
+    int age1 = parseint.JOptionPane.showInputDialog("What's Your Age?");
+    JOptionPane.showMessageDialog(null, "You Are " + age1);
+
+    //convo 3:
+    String favfood1 = JOptionPane.showInputDialog("What's Your Favorite Food?");
+    JOptionPane.showMessageDialog(null, "Your Favourite Food Is " + favfood1);
+
+    //convo end:
+    JOptionPane.showMessageDialog(null, "Hello " + name1 + ", You Are " + age1 + "Years Old, And You Like " + favfood1);
   }
 }
