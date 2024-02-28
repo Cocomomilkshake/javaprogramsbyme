7838e85373916c9f96038d360e2ab8d7ac0a0b40
Cocomomilkshake <76200257+Cocomomilkshake@users.noreply.github.com>
2024-02-28 19:17:41 +0800

    Here you go abdullah

---
+++ MultiplesOfFive.java
@@ -0,0 +1,8 @@
+public class MultiplesOfFive{
+	public static void main(String[] args){
+		for (int number = 5; number <= 100;) {
+			System.out.println(number);
+			number = number + 5;
+		}
+	}
+}
